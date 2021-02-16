b33f9fb7456082981ebc22a72b15422fc246e4d2
Govind Gnanakumar <govind.gnanakumar@outlook.com>
2021-02-16 11:32:14 -0800

    Fresh restart

--- src/main/kotlin/libkit/GitRepository.kt
+++ src/main/kotlin/libkit/GitRepository.kt
@@ -5,64 +5,22 @@ import java.io.File
 
 
 class GitRepository constructor(path: String = ".", force: Boolean = false) {
-    private var confReader: Wini
-    var worktree: File? = File(path.removeSuffix("/").removeSuffix("\\"))
+    private var conf: Wini
+    var worktree: File? = File(path)
     var gitdir: File? = File(this.worktree.toString() + "/.git")
 
+
     init {
-        if (!force || !this.gitdir!!.isDirectory)
+        if (!(force || this.gitdir!!.isDirectory))
             Utils.fail("Not a Git repository $path")
 
         val confPath = File("${this.gitdir}/config")
-        this.confReader = Wini(confPath)
+
+        if (!confPath.isFile)
+            Utils.fail("Configuration file missing")
 
 
-        /*
-        *
-        cf = repo_file(self, "config")
-
-        if cf and os.path.exists(cf):
-                self.conf.read([cf])
-        elif not force:
-            raise Exception("Configuration file missing")
-
-        if not force:
-            vers = int(self.conf.get("core", "repositoryformatversion"))
-            if vers != 0:
-                raise Exception("Unsupported repositoryformatversion %s" % vers)
-
-
-
-
-def repo_dir(repo, *path, mkdir=False):
-    """Same as repo_path, but mkdir *path if absent if mkdir."""
-
-    path = repo_path(repo, *path)
-
-    if os.path.exists(path):
-        if (os.path.isdir(path)):
-            return path
-        else:
-            raise Exception("Not a directory %s" % path)
-
-    if mkdir:
-        os.makedirs(path)
-        return path
-    else:
-        return None
-        * */
+        this.conf = Wini(confPath)
     }
-
-
 }
 
-fun repoPath(repo: GitRepository, vararg path: String) = path.forEach { repo.gitdir.toString() + "/" + it }
-
-fun repoFile(repo: GitRepository, vararg path: String, mkdir: Boolean = false) {
-    if (repoDir(repo, *path.sliceArray(0..path.size - 2), mkdir = mkdir)) return repoPath(repo, *path)
-}
-
-fun repoDir(repo: GitRepository, vararg path: String, mkdir: Boolean = false): Boolean {
-    val path: File = File(repoPath(repo, *path))
-    if ()
-}
--- src/main/kotlin/libkit/Utils.kt
+++ src/main/kotlin/libkit/Utils.kt
@@ -4,5 +4,6 @@ class Utils {
     companion object {
         fun fail(message: String): Nothing =
             throw IllegalArgumentException(message)
+
     }
 }
